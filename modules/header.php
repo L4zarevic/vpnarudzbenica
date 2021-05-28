d9579558167ca67b41b08f10da9cada23401f9ce
Nemanja-MO <lazarevic.nemanja@gmail.com>
2021-05-28 23:19:55 +0200

    Add stop propagation on deleteRow function, to prevent show bootstrap modal when click on delete button

--- modules/header.php
+++ modules/header.php
@@ -24,5 +24,5 @@ $imeKorisnika = $ar[1]; ?>
     <link rel="stylesheet" href="../css/themes/default.min.css" />
     <link rel="icon" type="image/png" href="../images/favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="../images/favicon-16x16.png" sizes="16x16" />
-    <link rel="icon" type="image/png" href="./images/favicon.ico" />
+    <link rel="icon" type="image/png" href="../images/favicon.ico" />
 </head>
